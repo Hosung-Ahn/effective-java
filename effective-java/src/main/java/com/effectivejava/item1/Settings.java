2850c074ed8cd84e3211bad41d756959bfde263f
Hosung <an3735297@pusan.ac.kr>
2023-07-01 15:43:17 +0900

    item1 : 장점 2 ) 호출할때마다 새로운 인스턴스를 생성하지 않아도 된다
    error code : 생성자로 호출하게 되면 생성될 때마다 새로운 인스턴스가 생성되는 것을 막을 수 없다.

---
+++ effective-java/src/main/java/com/effectivejava/item1/Settings.java
@@ -0,0 +1,13 @@
+package com.effectivejava.item1;
+
+public class Settings {
+    private boolean useAutoSteering;
+    private boolean useABS;
+    private Difficulty difficulty;
+
+    public static void main(String[] args) {
+        System.out.println(new Settings());
+        System.out.println(new Settings());
+        System.out.println(new Settings());
+    }
+}
