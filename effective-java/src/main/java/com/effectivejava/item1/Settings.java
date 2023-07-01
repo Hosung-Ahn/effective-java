65999d6a98e656bbf146110d0965657103f31cdc
Hosung <an3735297@pusan.ac.kr>
2023-07-01 19:33:32 +0900

    item1 : 장점 2)

--- effective-java/src/main/java/com/effectivejava/item1/Settings.java
+++ effective-java/src/main/java/com/effectivejava/item1/Settings.java
@@ -1,13 +1,25 @@
 package com.effectivejava.item1;
 
+import javax.lang.model.SourceVersion;
+
 public class Settings {
     private boolean useAutoSteering;
     private boolean useABS;
     private Difficulty difficulty;
 
+    private Settings() {}
+
+    private static final Settings SETTINGS = new Settings();
+
+    public static Settings newInstance() {
+        return SETTINGS;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Settings());
-        System.out.println(new Settings());
-        System.out.println(new Settings());
+        Settings settings1 = Settings.newInstance();
+        Settings settings2 = Settings.newInstance();
+
+        System.out.println("settings1 = " + settings1);
+        System.out.println("settings2 = " + settings2);
     }
 }
