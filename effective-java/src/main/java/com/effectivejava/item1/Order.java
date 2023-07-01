8bdc82ebf26d92df92ad8a47f28e6dfd4cb32ec7
Hosung <an3735297@pusan.ac.kr>
2023-07-01 15:12:09 +0900

    item1 : static factory method
    static factory method 를 활용해서 method 이름이 다른 2개의 생성자를 구분하여 만들었다.

--- effective-java/src/main/java/com/effectivejava/item1/Order.java
+++ effective-java/src/main/java/com/effectivejava/item1/Order.java
@@ -5,13 +5,17 @@ public class Order {
     private boolean urgent;
     private Product product;
 
-    public Order(Product product, boolean prime) {
-        this.product = product;
-        this.prime = prime;
+    public static Order primeOrder(Product product) {
+        Order order = new Order();
+        order.prime = true;
+        order.product = product;
+        return order;
     }
 
-    public Order(Product product, boolean urgent) {
-        this.product = product;
-        this.urgent = urgent;
+    public static Order urgentOrder(Product product) {
+        Order order = new Order();
+        order.urgent = true;
+        order.product = product;
+        return order;
     }
 }
