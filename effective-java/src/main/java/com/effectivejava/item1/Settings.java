1208567ae7666811d153bf92d75d1ef580f9945a
Hosung <an3735297@pusan.ac.kr>
2023-07-03 19:22:15 +0900

    item 1 : static factory method
    단점 2 : static factory method 는 프로그래머가 찾기 어렵다.
    이는 문서화를 잘해줌으로써 어느 정도 해결할 수 있다.

--- effective-java/src/main/java/com/effectivejava/item1/Settings.java
+++ effective-java/src/main/java/com/effectivejava/item1/Settings.java
@@ -1,7 +1,9 @@
 package com.effectivejava.item1;
 
-import javax.lang.model.SourceVersion;
-
+/**
+ * 이 클래스의 인스턴스는 getInstance() 를 통해 사용한다.
+ * @See #getInstance()
+ */
 public class Settings {
     private boolean useAutoSteering;
     private boolean useABS;
@@ -11,13 +13,13 @@ public class Settings {
 
     private static final Settings SETTINGS = new Settings();
 
-    public static Settings newInstance() {
+    public static Settings getInstance() {
         return SETTINGS;
     }
 
     public static void main(String[] args) {
-        Settings settings1 = Settings.newInstance();
-        Settings settings2 = Settings.newInstance();
+        Settings settings1 = Settings.getInstance();
+        Settings settings2 = Settings.getInstance();
 
         System.out.println("settings1 = " + settings1);
         System.out.println("settings2 = " + settings2);
