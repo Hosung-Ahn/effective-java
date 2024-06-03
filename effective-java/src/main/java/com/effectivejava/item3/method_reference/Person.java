1e8e69b686eed0ecc8965031e6df00d6ca19b38f
Hosung <an3735297@pusan.ac.kr>
2024-06-03 13:51:09 +0900

    item 7 : 메모리 누수가 발생하는 Stack 예시

--- effective-java/src/main/java/com/effectivejava/item3/method_reference/Person.java
+++ effective-java/src/main/java/com/effectivejava/item3/method_reference/Person.java
@@ -2,10 +2,7 @@ package com.effectivejava.item3.method_reference;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Person {
     LocalDate birthday;
@@ -25,13 +22,20 @@ public class Person {
         return this.birthday.compareTo(b.birthday);
     }
 
+    public int compare(Person a) {
+        return 1;
+    }
+
+
     public static void main(String[] args) {
         List<Person> arr = new ArrayList<>();
         arr.add(new Person(LocalDate.of(1965, 1, 25)));
         arr.add(new Person(LocalDate.of(1985, 1, 25)));
         arr.add(new Person(LocalDate.of(2005, 1, 25)));
 
+        arr.sort(Person::compare);
         arr.sort(Person::comparedByAge);
+        Person person = new Person(LocalDate.of(1965, 1, 25));
 
         List<LocalDate> dates = new ArrayList<>();
         dates.add(LocalDate.of(1965, 1, 25));
