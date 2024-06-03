d733953ad43b36d4a62b2405e37db36f287b83b3
Hosung <an3735297@pusan.ac.kr>
2024-06-03 14:41:13 +0900

    item 7 : cache 데이터에 대한 메모리 누수 / backgroundThread 로 해결

--- effective-java/src/test/java/com/effectivejava/item7/cache/PostRepositoryTest.java
+++ effective-java/src/test/java/com/effectivejava/item7/cache/PostRepositoryTest.java
@@ -3,6 +3,12 @@ package com.effectivejava.item7.cache;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +24,30 @@ class PostRepositoryTest {
         System.out.println("Run GC");
         System.gc();
         System.out.println("End GC");
+
+        assertThat(postRepository.getCache().isEmpty()).isTrue();
+    }
+
+    @Test
+    void backgroundThread() throws InterruptedException {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        PostRepository postRepository = new PostRepository();
+        postRepository.getPostById(1); // cache 데이터 생성
+
+        Runnable removeOldCache = () -> {
+            Map<CacheKey, Post> cache = postRepository.getCache();
+            // 가장 오래된 cache 데이터 삭제
+            cache.keySet().stream()
+                    .min(Comparator.comparing(CacheKey::getCreatedAt))
+                    .ifPresent((k) -> {
+                        System.out.println("Remove cache: " + k);
+                        cache.remove(k);
+                    });
+        };
+
+        executor.scheduleAtFixedRate(removeOldCache, 1, 3, TimeUnit.SECONDS);
+        Thread.sleep(5000L);
+        executor.shutdown();
 
         assertThat(postRepository.getCache().isEmpty()).isTrue();
     }
