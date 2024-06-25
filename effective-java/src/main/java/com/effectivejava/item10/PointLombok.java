e5ede95d12e3eefe37dc71e0f58b64cae2564593
HOSUNG <an3735297@pusan.ac.kr>
2024-06-25 15:22:40 +0900

    item 10 : record 로 hashCode, equals, toString 생성하기

--- effective-java/src/main/java/com/effectivejava/item10/PointLombok.java
+++ effective-java/src/main/java/com/effectivejava/item10/PointLombok.java
@@ -13,4 +13,15 @@ public class PointLombok {
         this.x = x;
         this.y = y;
     }
+
+    public static void main(String[] args) {
+        PointRecord pointRecord = new PointRecord(10, 20);
+        PointRecord pointRecord2 = new PointRecord(10, 20);
+
+        System.out.println(pointRecord.equals(pointRecord2)); // true
+        System.out.println(pointRecord2.equals(pointRecord)); // true
+
+        System.out.println(pointRecord); // PointRecord[x=10, y=20]
+        System.out.println(pointRecord.x()); // 10
+    }
 }
